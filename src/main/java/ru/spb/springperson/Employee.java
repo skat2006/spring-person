de696f0aab4fee980e8f535bf8794ff881cc996c
Andrew <skat2006@bk.ru>
2022-08-01 21:35:27 +0300

    Initial commit

---
+++ src/main/java/ru/spb/springperson/Employee.java
@@ -0,0 +1,41 @@
+package ru.spb.springperson;
+
+public class Employee {
+    private String lastName;
+    private String firstName;
+
+    public Employee(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + lastName;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Employee employee) {
+            return this.lastName.equals(employee.lastName) && this.firstName.equals(employee.firstName);
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/ru/spb/springperson/SpringPersonApplication.java
@@ -0,0 +1,13 @@
+package ru.spb.springperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPersonApplication.class, args);
+	}
+
+}
